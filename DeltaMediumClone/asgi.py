3465366988c36a55210fcec9faa5878175c5fcb0
adey-px <aosteph@gmail.com>
2023-06-04 02:06:02 -0500

    Changed django project name

--- DeltaMediumClone/asgi.py
+++ DeltaMediumClone/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MongiloMediumClone.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DeltaMediumClone.settings")
 
 application = get_asgi_application()
--- DeltaMediumClone/settings.py
+++ DeltaMediumClone/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "MongiloMediumClone.urls"
+ROOT_URLCONF = "DeltaMediumClone.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "MongiloMediumClone.wsgi.application"
+WSGI_APPLICATION = "DeltaMediumClone.wsgi.application"
 
 
 # Database
